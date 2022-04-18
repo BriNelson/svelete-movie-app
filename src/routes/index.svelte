ed1ef6996a6b9c7007019dc41c98ef9f0ff1bfd9
Brinelson <ladofmusic@gmail.com>
2022-04-17 23:55:37 -0600

    working with flowbite for css intial work

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { Input, Button } from 'flowbite-svelte'
+    </script>
+
+<Input class="mb-6" placeholder="Base input" label="Base input"/>
+<Button name="Button text-xs" textSize="text-xs" />
+<p>is this working</p>
+
