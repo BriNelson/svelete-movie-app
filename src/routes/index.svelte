87131b3d559d1cd1a6c2ce701f9863fd898390b9
Brinelson <ladofmusic@gmail.com>
2022-04-25 02:54:08 -0600

    working on getting info from api route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,25 @@
 <script>
-    
+    let searchedMovie = ''
+    let movieDataArray= handleClick()
+    async function handleClick() {
+        const response = await fetch(`https://api.watchmode.com/v1/autocomplete-search/?apiKey=2YFLc8ybj1SYc0aKKuuAmixVq0Gn0I5HeummEHHT&search_value=${searchedMovie}&search_type=1'`)
+        const movieData = await response.json()
+        return movieData
+        
+    }
 </script>
 
 <div>
     <nav class="navbar navbar-dark bg-primary">
         <div class="container-fluid">
           <a class="navbar-brand">Navbar</a>
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <form on:submit|preventDefault={handleClick} class="d-flex">
+            <input bind:value={searchedMovie} class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-light" type="submit">Search</button>
           </form>
         </div>
       </nav>
-<p>is this working</p>
-<button type="button" class="btn btn-primary">Primary</button>
+      
+
 
 </div>
