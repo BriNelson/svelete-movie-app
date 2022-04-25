04618ddec3c8409bb7f0eecc5d035bec30be37f7
Brinelson <ladofmusic@gmail.com>
2022-04-25 01:11:15 -0600

    navbar added bootstrap working

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,18 @@
 <script>
     
-    </script>
+</script>
 
-<div class="container mx-auto">
+<div>
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand">Navbar</a>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-light" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
 <p>is this working</p>
-
+<button type="button" class="btn btn-primary">Primary</button>
 
 </div>
