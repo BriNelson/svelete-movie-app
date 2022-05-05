30d2f7d1bc6e6348a38a7ea4c1d98b510c1ece34
Brinelson <ladofmusic@gmail.com>
2022-05-05 01:31:50 -0600

    home route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,46 @@
+<!-- SHOWS ROUTE -->
+
 <script>
- 
+    import FavoriteStore from '../stores/FavoriteStore.js'
+    import { fade, scale } from 'svelte/transition'
+    import { flip } from 'svelte/animate'
 
-   
+    let favorites = []
+    FavoriteStore.subscribe(data =>{  
+favorites = data
 
+    });
+
+    
+
+    const handleDelete = (id) => {
+FavoriteStore.update(current => {
+return current.filter(favorite => favorite.id != id)
+
+})
+    }
 </script>
 
-<div>
-    
-    <div class="container">
-        <h1 class="mt-2">Home</h1>
-        </div>
- 
 
+
+<div class="container">
+    <h1 class="mt-2" in:fade>Home</h1>
+
+      <div class="row row-cols-1 row-cols-md-6 g-4 mt-3">
+    {#each favorites as favorite}
+    
+    <div class="col">
+        <div class="card h-100" in:fade out:scale>
+          <img src={favorite.image_url} class="card-img-top" alt="...">
+          <div class="card-body">
+            <h6 class="card-title">{favorite.name}</h6>
+            <h6 class="card-subtitle mb-2 text-muted">{favorite.year}</h6>
+          </div>
+          <div class="card-footer"><button class="btn btn-outline-primary"><a href={`/movies/${favorite.id}`}>info</a></button><button class="btn btn-danger ms-2" on:click={() => handleDelete(favorite.id)}>X</button></div>
+        </div>
+      </div>
+
+         
+    {/each}
+</div>
 </div>
